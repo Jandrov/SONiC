eef8b099b19ff3071a57a727bda04cca2713fd68
root <root@javiergalan.home>
2019-05-09 22:05:49 +0200

    imagen de ubuntu

--- load_image.sh
+++ load_image.sh
@@ -1,2 +1,4 @@
 wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/543/artifact/target/docker-sonic-p4.gz
+wget http://releases.ubuntu.com/16.04/ubuntu-16.04.6-desktop-amd64.iso
 sudo docker load < docker-sonic-p4.gz
+sudo docker load < ubuntu-16.04.6-desktop-amd64.iso
