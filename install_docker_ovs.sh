68f17b425f4f8db4f85a80b01ef2a100f22c22d3
jgalans <galansjavier@gmail.com>
2019-05-14 21:12:06 +0200

    Update install_docker_ovs.sh

--- install_docker_ovs.sh
+++ install_docker_ovs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+#sudo apt-get update
+#sudo apt-get install -y --no-install-recommends \
+#    apt-transport-https \
+#    ca-certificates \
+#    curl \
+#    software-properties-common
+#curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+#apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+#sudo add-apt-repository \
+#       "deb https://apt.dockerproject.org/repo/ \
+#       ubuntu-$(lsb_release -cs) \
+#       main"
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
-apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
-sudo add-apt-repository \
-       "deb https://apt.dockerproject.org/repo/ \
-       ubuntu-$(lsb_release -cs) \
-       main"
-sudo apt-get update
-sudo apt-get -y install docker-engine
+#sudo apt-get -y install docker-engine
 
 sudo apt-get install -y openvswitch-switch
 sudo curl -L https://github.com/openvswitch/ovs/raw/master/utilities/ovs-docker -o /usr/bin/ovs-docker
