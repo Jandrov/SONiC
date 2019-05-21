06ae324598a255cf1fced86d2e9d530b594f3404
jgalans <galansjavier@gmail.com>
2019-05-21 19:47:32 +0200

    Update load_image.sh

--- load_image.sh
+++ load_image.sh
@@ -1,5 +1,6 @@
 wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/543/artifact/target/docker-sonic-p4.gz
 sudo docker load < docker-sonic-p4.gz
+sudo docker --pull ubuntu
 
 #Contruimos las imagenes atendiendo al Dockerfile
 sudo docker build -t ubuntu
