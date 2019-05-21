ed5300691ace14b4138c05e5d0d0cfefc0671dc5
jgalans <galansjavier@gmail.com>
2019-05-21 19:42:03 +0200

    Update load_image.sh

--- load_image.sh
+++ load_image.sh
@@ -1,3 +1,4 @@
+sudo docker images
 wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/543/artifact/target/docker-sonic-p4.gz
 sudo docker load < docker-sonic-p4.gz
 
