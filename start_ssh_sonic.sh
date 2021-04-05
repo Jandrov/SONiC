16c89892dc05767b1092c42b7ccfbb8d346af9fb
Jandrov <jandrov.97@gmail.com>
2021-04-05 18:13:22 +0200

    Update start_ssh_sonic.sh to run on Mac

--- start_ssh_sonic.sh
+++ start_ssh_sonic.sh
@@ -2,12 +2,21 @@
 
 NAME=sonic-ssh
 
+# Check correct parameters
 if [ ! $1 ]; then
     echo "[ERROR] Usage is: 'source start_ssh_sonic.sh <root_password>'"
     return
 fi
 
-sudo docker run --privileged --entrypoint /bin/bash --name $NAME -it -d -v $PWD/switch1:/sonic docker-sonic-p4:latest
+KERNEL_NAME=$(uname)
+
+# Docker on Mac requires mapping a different port to standard SSH port
+if [[ "$KERNEL_NAME" == "Darwin" ]]; then
+    sudo docker run --privileged --entrypoint /bin/bash --name $NAME -it -d -p 2022:22 -v $PWD/switch1:/sonic docker-sonic-p4:latest
+else
+    sudo docker run --privileged --entrypoint /bin/bash --name $NAME -it -d -v $PWD/switch1:/sonic docker-sonic-p4:latest
+fi
+
 sudo docker exec -d $NAME sh /sonic/scripts/startup.sh
 
 sleep 60
@@ -19,6 +28,14 @@ sudo docker exec -d $NAME service ssh restart
 
 sleep 10
 
-export SONIC_ADDR=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" $NAME)
-echo "Container $NAME created and running with $SONIC_ADDR IP address"
+# Docker on Mac exposes container in localhost
+if [[ "$KERNEL_NAME" == "Darwin" ]]; then
+    export SONIC_ADDR=localhost
+    export SONIC_PORT=2022
+else
+    export SONIC_ADDR=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" $NAME)
+    export SONIC_PORT=22
+fi
+
+echo "Container $NAME created and running with $SONIC_ADDR IP address and SSH port $SONIC_PORT"
 
