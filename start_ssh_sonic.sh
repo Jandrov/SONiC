7e1a6c91e1f42f7fe8bf3d7958fd53539680ef21
Jandrov <frinx.tu.contributor@gmail.com>
2021-04-17 23:13:01 +0200

    Update ssh scripts

--- start_ssh_sonic.sh
+++ start_ssh_sonic.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-NAME=sonic-ssh
+NAMES=(switch1-ssh switch2-ssh)
+NETWORK=my-network
+KERNEL_NAME=$(uname)
 
 # Check correct parameters
 if [ ! $1 ]; then
@@ -8,34 +10,62 @@ if [ ! $1 ]; then
     return
 fi
 
-KERNEL_NAME=$(uname)
 
 # Docker on Mac requires mapping a different port to standard SSH port
 if [[ "$KERNEL_NAME" == "Darwin" ]]; then
-    sudo docker run --privileged --entrypoint /bin/bash --name $NAME -it -d -p 2022:22 -v $PWD/switch1:/sonic docker-sonic-p4:latest
+    sudo docker run --privileged --entrypoint /bin/bash --name ${NAMES[0]} -it -d -p 2022:22 -v $PWD/switch1:/sonic docker-sonic-p4:latest
+    sudo docker run --privileged --entrypoint /bin/bash --name ${NAMES[1]} -it -d -p 2023:22 -v $PWD/switch2:/sonic docker-sonic-p4:latest
 else
-    sudo docker run --privileged --entrypoint /bin/bash --name $NAME -it -d -v $PWD/switch1:/sonic docker-sonic-p4:latest
+    sudo docker run --privileged --entrypoint /bin/bash --name ${NAMES[0]} -it -d -v $PWD/switch1:/sonic docker-sonic-p4:latest
+    sudo docker run --privileged --entrypoint /bin/bash --name ${NAMES[1]} -it -d -v $PWD/switch2:/sonic docker-sonic-p4:latest
 fi
 
-sudo docker exec -d $NAME sh /sonic/scripts/startup.sh
+for name in "${NAMES[@]}"; do
+    echo "Starting $name"
+    sudo docker exec -d $name sh /sonic/scripts/startup.sh
+done
 
-sleep 60
+sleep 20
 sudo docker ps
 
-sudo docker exec -d $NAME bash -c "echo 'root:$1' | chpasswd"
-sudo docker exec -d $NAME sed -i 's/without-password/yes/' /etc/ssh/sshd_config
-sudo docker exec -d $NAME service ssh restart
+for name in "${NAMES[@]}"; do
+    echo "Enabling SSH access in $name"
+    sudo docker exec -d $name bash -c "echo 'root:$1' | chpasswd"
+    sudo docker exec -d $name sed -i 's/without-password/yes/' /etc/ssh/sshd_config
+    sudo docker exec -d $name service ssh restart
+done
 
 sleep 10
 
 # Docker on Mac exposes container in localhost
 if [[ "$KERNEL_NAME" == "Darwin" ]]; then
-    export SONIC_ADDR=localhost
-    export SONIC_PORT=2022
+    export SWITCH1_ADDR=localhost
+    export SWITCH2_ADDR=localhost
+    export SWITCH1_PORT=2022
+    export SWITCH2_PORT=2023
 else
-    export SONIC_ADDR=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" $NAME)
-    export SONIC_PORT=22
+    export SWITCH1_ADDR=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" ${NAMES[0]})
+    export SWITCH2_ADDR=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" ${NAMES[1]})
+    export SWITCH1_PORT=22
+    export SWITCH2_PORT=22
 fi
 
-echo "Container $NAME created and running with $SONIC_ADDR IP address and SSH port $SONIC_PORT"
 
+# Create our own network and connect the switches to that network
+sudo docker network create $NETWORK
+for name in "${NAMES[@]}"; do
+    echo "Adding $name to the network $NETWORK"
+    sudo docker network connect $NETWORK $name
+done
+
+# Check IP addresses from our network assigned to each container
+ADDRESSES=($SWITCH1_ADDR $SWITCH2_ADDR)
+PORTS=($SWITCH1_PORT $SWITCH2_PORT)
+i=0
+sudo docker network inspect -f "{{ .Containers }}" $NETWORK | grep -Eo '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2}' | \
+    while read ip; do \
+        echo "Container ${NAMES[$i]} created and running with ${ADDRESSES[$i]} IP external address, $ip internal address and SSH port ${PORTS[$i]}"; \
+        ((i++)); \
+    done
+
+echo "Process has finished"
--- stop_ssh_sonic.sh
+++ stop_ssh_sonic.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-NAME=sonic-ssh
+SWITCH1=switch1-ssh
+SWITCH2=switch2-ssh
+NETWORK=my-network
 
-if [[ $(sudo docker container ls -a | grep -o $NAME) ]]; then
-    sudo docker rm -f $NAME
-    echo "Container $NAME has been removed"
+if [[ $(sudo docker container ls -a | grep -o $SWITCH1) ]]; then
+    sudo docker rm -f $SWITCH1
+    echo "Container $SWITCH1 has been removed"
 else
-    echo "[ERROR] There is no container with name $NAME"
+    echo "[ERROR] There is no container with name $SWITCH1"
 fi
+
+if [[ $(sudo docker container ls -a | grep -o $SWITCH2) ]]; then
+    sudo docker rm -f $SWITCH2
+    echo "Container $SWITCH2 has been removed"
+else
+    echo "[ERROR] There is no container with name $SWITCH2"
+fi
+
+sudo docker network rm $NETWORK
+echo "Network $NETWORK has been removed"
